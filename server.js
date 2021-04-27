81d23b718595bef478c22a402fa4a4c85f16fdf8
Michael Stevan Lapandio <michael.lapandio@xapiens.id>
2021-04-27 13:29:13 +0700

    [kyomel] solved

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-var express = require('express');
-var cors = require('cors');
+'use strict'
+
+let express = require('express');
+let cors = require('cors');
+let multer = require('multer');
 require('dotenv').config()
 
+let storage = multer.memoryStorage();
+let upload = multer({ storage: storage });
 var app = express();
 
 app.use(cors());
@@ -11,7 +16,18 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  res.json({
+    'name': req.file.originalname,
+    'type': req.file.mimetype,
+    'size': req.file.size
+  });
+});
 
+app.use(function(req, res, next) {
+  res.status(404);
+  res.type('txt').send('Not Found');
+})
 
 
 const port = process.env.PORT || 3000;
